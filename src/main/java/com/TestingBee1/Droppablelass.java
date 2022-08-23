98e0cb92e82189d6f2659654fe12ea791b931be7
Golam Tashin <taslimabegum395@gmail.com>
2022-08-23 17:08:52 -0400

    Selenium drag & drop

--- src/main/java/com/TestingBee1/Droppablelass.java
+++ src/main/java/com/TestingBee1/Droppablelass.java
@@ -13,7 +13,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Droppablelass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		
 		WebDriverManager.chromedriver().setup();
@@ -22,16 +22,21 @@ public class Droppablelass {
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://jqueryui.com/droppable/");
+		WebElement frame= driver.findElement(By.tagName("iframe"));
+		 List<WebElement>totalframe =driver.findElements(By.tagName("iframe"));
+		    totalframe.size();
+		    System.out.println(	totalframe.size());
+			driver.switchTo().frame(frame);
 		WebElement source =driver.findElement(By.xpath("//div[@id='draggable']"));
-		WebElement target = driver.findElement(By.xpath("/div[@id='droppable']"));
+		WebElement target = driver.findElement(By.xpath("//div[@id='droppable']"));
 		Actions ac=new Actions(driver);
 		ac.dragAndDrop(source, target).build().perform();
-	WebElement frame= driver.findElement(By.tagName("iframe"));
-	List<WebElement>totalframe =driver.findElements(By.tagName("iframe"));
-	totalframe.size();
-	System.out.println(	totalframe.size());
-		driver.switchTo().frame(frame);
+		driver.switchTo().defaultContent();
+	   Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[@href='http://jqueryui.com/draggable/']")).click();
 		
+		Thread.sleep(3000);
+		driver.quit();
 		
 		
 			
